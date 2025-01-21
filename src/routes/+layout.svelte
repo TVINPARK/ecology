77136b2acc521d1ad3d9a133120d3fd6607f1eb9
Arseniy <arsenijbojko35@gmail.com>
2025-01-21 21:31:08 +0500

    finish

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,49 @@
+<script>
+    import logo from "$lib/assets/logo.png"
+    const nav = [
+        {
+            title: "г. Златоуст",
+            href: "/zlat",
+            isShow: true,
+        },
+        {
+            title: "Саткинский МР",
+            href: "/sat",
+            isShow: true,
+        },
+        {
+            title: "Ашинский район",
+            href: "/aha",
+            isShow: true,
+        },
+        {
+            title: "Трёхгорный ГО",
+            href: "/treh",
+            isShow: true,
+        },
+        {
+            title: "Катаав-Ивановский МР",
+            href: "/kat",
+            isShow: true,
+        },
+        {
+            title: "Усть-Катавский ГО",
+            href: "/ustkat",
+            isShow: true,
+        },
+    ];
+</script>
+
+<nav>
+    <div class="nav-container">
+        <img src="{logo}" alt="trash">
+        <a href="/" class="nav-logo" title="Back to Homepage">Trash map</a>
+        <div class="nav-links">
+            {#each nav as link}
+                {#if link.isShow}
+                    <a href={link.href} class="link">{link.title}</a>
+                {/if}
+            {/each}
+        </div>
+    </div>
+</nav>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,81 @@
+<svelte:head>
+    <title>Арсений Бойко</title>
+</svelte:head>
+<div class="container">
+    <div class="info">
+        <div class="animate-background">
+            <div class="container info-container">
+                <div class="info-text">
+                    <h1>
+                        <p class="animate-text1">Я Бойко Арсений,</p>
+                        <p class="text-2">junior Frontend разработчик.</p>
+                    </h1>
+                </div>
+                <h2 class="subtitle">
+                    Я ученик 10 класса уже 3 год обучаюсь Frontend-разработке и сейчас ищу работу для получения опыта и чтобы накопить на профильный вуз
+                </h2>
+                <!-- <div style="color:black" class="scroll-button animate-scroll" tabindex="0">
+                    <a href="/about">Click Here</a>
+                    <div class="arrow">→</div>
+                </div> -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- <div class="background-div" /> -->
+
+<style>
+    .info {
+        text-align: center;
+        position: relative;
+    }
+    .info-container {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: left;
+        align-items: start;
+    }
+    .container {
+        max-width: 1400px;
+        margin: 190px 0 0 2.5rem;
+    }
+    .info h1 {
+        font-size: 4rem;
+        margin: 0;
+    }
+    .info h1 p {
+        position: relative;
+        font: inherit;
+        margin: 0;
+        text-align: left;
+        line-height: 130%;
+        width: fit-content;
+        font-weight: 800;
+        letter-spacing: -0.03em;
+    }
+    .info h2 {
+        max-width: 65ch;
+        font-weight: 200;
+        margin-top: 0.5rem;
+    }
+    /* .background-div {
+        background-color: #ecebe8;
+        width: 100%;
+        height: 56vh;
+        margin: -205px auto;
+    } */
+    .info .info-text {
+        position: relative;
+    }
+    /* .scroll-button {
+        display: flex;
+        border: none;
+        outline: none;
+        cursor: pointer;
+    } */
+    /* .scroll-button .arrow {
+        margin-left: 0.5rem;
+    } */
+</style>
